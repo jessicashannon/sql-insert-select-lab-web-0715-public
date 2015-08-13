094a388005d1e9352714cdde3fac3188590755a9
jessicashannon <jessdahling@gmail.com>
2015-08-13 17:47:49 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO cats 
+VALUES (1, 'Maru', 
+  6, 'M', 'Scottish Fold', 'playful', 0),
+(2, 'Nala', 3, 'F', 'Siamese', 'diva', 0),
+(3, 'Grumpy Cat', 2, 'F', 'Ragdoll','friendly', 1),
+(4, 'Lil Bub', 3, 'F', 'Dwarf', 'friendly', 1);
+
